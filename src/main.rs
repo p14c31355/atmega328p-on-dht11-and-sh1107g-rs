bf532954ad35f1f81c0eb8e340660954eb278e2e
Placeless <thinkoverheat@gmail.com>
2025-08-01 04:48:10 +0900

    Grove - Temperature&Humidity Sensor を Grove-Base Shield のポート D2 に接続します。 Grove - OLED Display 1.12''をGroveケーブルを使ってBase ShieldのI2Cポートに接続します。
    [dependencies]
    panic-halt = "1.0.0"
    ufmt = "0.2.0"
    nb = "1.1.0"
    embedded-hal = "1.0"
    embedded-dht-rs = "0.5.0"
    oled_async = "0.2.0"
    
    [dependencies.arduino-hal]
    git = "https://github.com/rahix/avr-hal"
    rev = "fafaf587a32a4500239fd073f89d1b9c36b48092"
    features = ["arduino-uno"]
    
    このとき，type Display = oled_async::displays::sh1107::Sh1107_128_128;
    と embedded-dht-rs を用いて温湿度センサーで読み取った温度と湿度をディスプレイに表示したいです

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,66 @@
 #![no_std]
 #![no_main]
 
+use arduino_hal::prelude::*;
 use panic_halt as _;
+use embedded_dht_rs::{Dht11, DhtReading};
+use oled_async::prelude::*;
+use oled_async::interface::I2CDisplayInterface;
+use oled_async::displays::sh1107::Sh1107_128_128;
+use arduino_hal::i2c::I2c;
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    // I2C初期化
+    let i2c = arduino_hal::I2c::new(
+        dp.TWI,
+        pins.a4.into_pull_up_input(), // SDA
+        pins.a5.into_pull_up_input(), // SCL
+        400_000,
+    );
 
-    let mut led = pins.d13.into_output();
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Sh1107_128_128::new(interface).into_buffered_graphics_mode();
+    display.init().unwrap();
+    display.flush().unwrap();
+
+    // DHT11 on D2
+    let dht_pin = pins.d2.into_open_drain_output();
+    let mut dht = Dht11::new(dht_pin);
+
+    // UART for debugging (optional)
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
     loop {
-        led.toggle();
-        arduino_hal::delay_ms(1000);
+        match dht.read() {
+            Ok(DhtReading { temperature, relative_humidity }) => {
+                ufmt::uwriteln!(
+                    &mut serial,
+                    "Temp: {} C, Hum: {} %\r",
+                    temperature,
+                    relative_humidity
+                )
+                .ok();
+
+                display.clear();
+                use core::fmt::Write;
+                use oled_async::graphics::GraphicsMode;
+                let _ = write!(
+                    display,
+                    "Temp:{}C\nHum:{}%",
+                    temperature,
+                    relative_humidity
+                );
+                display.flush().unwrap();
+            }
+            Err(e) => {
+                ufmt::uwriteln!(&mut serial, "Read error: {:?}\r", e).ok();
+            }
+        }
+
+        arduino_hal::delay_ms(2000);
     }
 }

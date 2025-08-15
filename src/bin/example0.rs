3a579c8e1b72a6276e0cff0aae0d09cc35b28bdb
Placeless <thinkoverheat@gmail.com>
2025-08-15 22:25:23 +0900

    error

--- src/bin/example0.rs
+++ src/bin/example0.rs
@@ -1,21 +1,47 @@
 #![no_std]
 #![no_main]
-#![feature(abi_avr_interrupt)]
 
-use arduino_hal::default_serial;
+use arduino_hal::hal::port::Dynamic;
+use arduino_hal::port::{mode, Pin};
+use arduino_hal::Delay;
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    text::{Baseline, Text},
+};
 use panic_halt as _;
+use sh1107g_rs::{prelude::*, Sh1107g};
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let mut serial = default_serial!(dp, pins, 57600);
+    let mut delay = Delay::new();
 
-    let mut i2c = arduino_hal::I2c::new(
+    let i2c = arduino_hal::I2c::new(
         dp.TWI,
         pins.a4.into_pull_up_input(),
         pins.a5.into_pull_up_input(),
         50000,
     );
+
+    let mut display = Sh1107g::new(I2CDisplayInterface::new(i2c), DisplaySize128x64, VccSource::External)
+        .into_buffered_graphics_mode();
+
+    display.init(&mut delay).unwrap();
+    display.clear();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    Text::with_baseline("Hello, World!", Point::new(0, 16), text_style, Baseline::Top)
+        .draw(&mut display)
+        .unwrap();
+
+    display.flush().unwrap();
+
     loop {}
 }

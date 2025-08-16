50d1a210302ae60e946016140fd3b494dfc34143
Placeless <thinkoverheat@gmail.com>
2025-08-16 23:26:12 +0900

    Cline に dvcdbg 0.1.2 使わせてみる

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -3,57 +3,45 @@
 #![feature(abi_avr_interrupt)]
 
 use arduino_hal::default_serial;
-use embedded_graphics::{
-    pixelcolor::BinaryColor,
-    prelude::*,
-};
+use embedded_graphics::pixelcolor::BinaryColor;
 use panic_halt as _;
 use sh1107g_rs::Sh1107g;
 use dvcdbg::prelude::*;
-use core::fmt::Write;
-
 use dvcdbg::scanner::scan_i2c;
 
-// dvcdbg::adapt_serial!マクロを使用してUsartAdapterを定義し、core::fmt::Writeを実装
-dvcdbg::adapt_serial!(avr_usart: UsartAdapter, write_byte);
+// マクロで UsartAdapter を定義
+adapt_serial!(@impls UsartAdapter, write_byte, <U, RX, TX, CLOCK>);
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let serial = default_serial!(dp, pins, 57600); // serialを可変にしない
+    let serial = default_serial!(dp, pins, 57600);
 
-    let mut dbg_uart = dvcdbg::UsartAdapter(serial); // UsartAdapterでラップ
-    let mut logger = dvcdbg::SerialLogger::new(&mut dbg_uart); // ラップされたインスタンスを使用
+    let mut dbg_uart = UsartAdapter(serial); // ← crate 経由ではなく直接
+    let mut logger = SerialLogger::new(&mut dbg_uart); // prelude に export されてる
 
-    let mut i2c = arduino_hal::I2c::new( // i2c を可変にする
+    let mut i2c = arduino_hal::I2c::new(
         dp.TWI,
         pins.a4.into_pull_up_input(),
         pins.a5.into_pull_up_input(),
         50000,
     );
 
-    // I2Cバスのスキャンを実行
     scan_i2c(&mut i2c, &mut logger);
 
     let mut display = Sh1107g::new(&mut i2c, 0x3C);
 
-    // log! マクロの呼び出しを display.with_logger でラップ
-    // `Sh1107g` に `with_logger` メソッドを追加
     log!(logger, "Initializing...");
-
     display.init().unwrap();
     display.clear(BinaryColor::Off).unwrap();
 
     log!(logger, "Display initialized and cleared.");
-    
 
     display.clear(BinaryColor::On).unwrap();
     display.flush().unwrap();
-    
-        log!(logger, "Display filled with white.");
 
-    loop {
-        // 無限ループ
-    }
+    log!(logger, "Display filled with white.");
+
+    loop {}
 }

5dadfba82d10e3f6da9fe270022874972d79c02a
Placeless <thinkoverheat@gmail.com>
2025-08-16 10:31:33 +0900

    Cline に dvcdbg 0.1.2 使わせてみる

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -9,13 +9,12 @@ use embedded_graphics::{
 };
 use panic_halt as _;
 use sh1107g_rs::Sh1107g;
-use dvcdbg::logger::SerialLogger;
-use dvcdbg::log;
 use dvcdbg::prelude::*;
 use core::fmt::Write;
 
 use dvcdbg::scanner::scan_i2c;
 
+impl_fmt_write_for_serial!(arduino_hal::hal::usart::Usart, write_byte);
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -35,7 +34,7 @@ fn main() -> ! {
     // I2Cバスのスキャンを実行
     scan_i2c(&mut i2c, &mut logger);
 
-    let mut display = Sh1107g::new(i2c, 0x3C, Some(&mut logger));
+    let mut display = Sh1107g::new(&mut i2c, 0x3C);
 
     // log! マクロの呼び出しを display.with_logger でラップ
     // `Sh1107g` に `with_logger` メソッドを追加

79f07d6be6b099d9e0e5c26a8112aa167a24f597
Placeless <thinkoverheat@gmail.com>
2025-08-18 01:49:25 +0900

    error

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -1,29 +1,23 @@
 #![no_std]
 #![no_main]
 
-use core::fmt::Write; // writeln! 用
+use core::fmt::Write;
 use panic_halt as _;
+
 use dvcdbg::adapt_serial;
-use arduino_hal::prelude::*;
-use arduino_hal::default_serial;
 
-// embedded-io::Write と core::fmt::Write の衝突回避
-use embedded_io::Write as IoWrite;
-
-adapt_serial!(UsartAdapter, nb_write = write, flush = flush);
+adapt_serial!(UsartAdapter, nb_write = write, flush = flush); // NB1.1 + embedded-io 0.6 対応
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let serial = default_serial!(dp, pins, 57600);
 
+    // 57600bps の UART
+    let serial = arduino_hal::default_serial!(dp, pins, 57600);
     let mut dbg_uart = UsartAdapter(serial);
 
-    // core::fmt::Write 経由で文字列出力
     writeln!(dbg_uart, "Hello from embedded-io bridge!").ok();
-
-    // embedded_io::Write 経由でバイト列送信
     dbg_uart.write_all(&[0x01, 0x02, 0x03]).ok();
 
     loop {}

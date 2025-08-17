de27ef75f566530d145400932ac6447b249a3024
Placeless <thinkoverheat@gmail.com>
2025-08-17 18:40:56 +0900

    刑罰

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -1,21 +1,27 @@
 #![no_std]
 #![no_main]
 
-use panic_halt as _; // panic時は停止
-use dvcdbg::adapt_serial;
-use dvcdbg::prelude::*;
-use arduino_hal::default_serial;
+use arduino_hal::prelude::*;
+use arduino_hal::Usart;
+use dvcdbg::adapt_serial; // マクロをインポート
+use panic_halt as _;
+
+adapt_serial!(UsartAdapter);
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let serial = default_serial!(dp, pins, 57600);
 
-    adapt_serial!(UsartAdapter);
+    // USART 初期化
+    let serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    // マクロで作られたアダプタを使う
     let mut dbg_uart = UsartAdapter(serial);
-    let mut logger = SerialLogger::new(&mut dbg_uart);
 
-    log!(logger, "Hello from AVR!");
+    // write! マクロが使える
+    use core::fmt::Write;
+    writeln!(dbg_uart, "Hello from dvcdbg!").ok();
+
     loop {}
 }

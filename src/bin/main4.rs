31330419e1307df9457acc4b1c4dadceb31ce3f0
Placeless <thinkoverheat@gmail.com>
2025-08-18 01:15:29 +0900

    error

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -2,22 +2,26 @@
 #![no_main]
 
 use core::fmt::Write;
-use core::convert::Infallible;
 use panic_halt as _;
 use dvcdbg::adapt_serial;
-use arduino_hal::prelude::*;
 
-adapt_serial!(UsartAdapter, nb_write = write, error = Infallible);
+adapt_serial!(UsartAdapter, nb_write = write, flush);
 
 #[arduino_hal::entry]
 fn main() -> ! {
+    // Peripherals とピンの取得
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
+    // 標準シリアルを作成
     let serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    // マクロで生成されたアダプタにラップ
     let mut dbg_uart = UsartAdapter(serial);
 
+    // 書き込みテスト
     writeln!(dbg_uart, "Hello from embedded-io bridge!").ok();
+    dbg_uart.write_all(&[0x01, 0x02, 0x03]).ok();
 
     loop {}
 }

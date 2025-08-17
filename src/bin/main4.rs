35d23b2a48f1c2c65ed05a90d4502f1ad623ae2e
Placeless <thinkoverheat@gmail.com>
2025-08-17 18:42:10 +0900

    刑罰

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -3,8 +3,8 @@
 
 use arduino_hal::prelude::*;
 use arduino_hal::Usart;
-use dvcdbg::adapt_serial; // マクロをインポート
-use panic_halt as _;
+use dvcdbg::adapt_serial;
+use core::fmt::Write;
 
 adapt_serial!(UsartAdapter);
 
@@ -13,15 +13,10 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    // USART 初期化
     let serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    // マクロで作られたアダプタを使う
     let mut dbg_uart = UsartAdapter(serial);
-
-    // write! マクロが使える
-    use core::fmt::Write;
-    writeln!(dbg_uart, "Hello from dvcdbg!").ok();
+    writeln!(dbg_uart, "Hello!").ok();
 
     loop {}
 }

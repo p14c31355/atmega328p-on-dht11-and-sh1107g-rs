b93b87a4b70316782b830eab336851929eb1cddd
Placeless <thinkoverheat@gmail.com>
2025-08-17 20:14:24 +0900

    cargo update

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -5,7 +5,7 @@ use panic_halt as _;
 use dvcdbg::adapt_serial;
 use core::fmt::Write as _;
 
-adapt_serial!(UsartAdapter, nb_write = write);
+adapt_serial!(UsartAdapter, nb_write = write, error = nb::Error<Infallible>, flush = flush);
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -13,8 +13,9 @@ fn main() -> ! {
     let pins = arduino_hal::pins!(dp);
 
     let serial = arduino_hal::default_serial!(dp, pins, 57600);
-
     let mut dbg_uart = UsartAdapter(serial);
+
     writeln!(dbg_uart, "Hello from embedded-io bridge!").ok();
+
     loop {}
 }

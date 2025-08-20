ebae9110b435642015a66d4018b0ce27a07e7ce0
Placeless <thinkoverheat@gmail.com>
2025-08-21 02:30:04 +0900

    error

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -1,25 +1,23 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::prelude::*;
 use panic_halt as _;
+use arduino_hal::prelude::*;
 use core::fmt::Write;
 
 use dvcdbg::prelude::*;
-
-adapt_serial!(SerialAdapter);
+adapt_serial!(UnoSerial);
 
 #[arduino_hal::entry]
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::default_pins!(dp);
-    let serial = arduino_hal::default_serial!(dp, pins, 57600);
-    
-    // SerialCompatを実装する型を`SerialAdapter`でラップ
-    let mut serial_logger = SerialAdapter(serial);
+    let pins = arduino_hal::pins!(dp);
 
-    // これで`writeln!`マクロを使用できる
-    writeln!(serial_logger, "Hello from dvcdbg on Arduino Uno!").unwrap();
+    let serial = arduino_hal::default_serial!(dp, pins, 57600);
+
+    let mut logger = UnoSerial(serial);
+
+    writeln!(logger, "Hello from dvcdbg on Arduino Uno!").unwrap();
 
     loop {}
 }

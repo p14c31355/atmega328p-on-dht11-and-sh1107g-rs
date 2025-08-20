c6b916bf8ee9012cff92d2294ffc5bb29c7c7c2e
Placeless <thinkoverheat@gmail.com>
2025-08-21 02:03:34 +0900

    error

--- src/bin/main4.rs
+++ src/bin/main4.rs
@@ -7,21 +7,19 @@ use core::fmt::Write;
 
 use dvcdbg::prelude::*;
 
-adapt_serial!(UnoSerial);
+adapt_serial!(SerialAdapter);
 
 #[arduino_hal::entry]
 fn main() -> ! {
-
     let dp = arduino_hal::Peripherals::take().unwrap();
-    let pins = arduino_hal::pins!(dp);
-
+    let pins = arduino_hal::default_pins!(dp);
     let serial = arduino_hal::default_serial!(dp, pins, 57600);
+    
+    // SerialCompatを実装する型を`SerialAdapter`でラップ
+    let mut serial_logger = SerialAdapter(serial);
 
-    let mut serial_logger = UnoSerial(serial);
+    // これで`writeln!`マクロを使用できる
+    writeln!(serial_logger, "Hello from dvcdbg on Arduino Uno!").unwrap();
 
-    writeln!(serial_logger, "Hello from dvcdbg on Arduino Uno!");
-
-    loop {
-        // your code
-    }
+    loop {}
 }

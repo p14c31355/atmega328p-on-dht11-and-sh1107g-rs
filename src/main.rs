ec2d9904b00f27f9f454b0aafd2d1a9057f7a948
Placeless <thinkoverheat@gmail.com>
2025-08-26 03:46:18 +0900

    silence

--- src/main.rs
+++ src/main.rs
@@ -3,66 +3,25 @@
 
 use arduino_hal::prelude::*;
 use arduino_hal::i2c;
-use dvcdbg::{
-    adapt_serial,
-    explorer::{Explorer, CmdNode, CmdExecutor},
-    scanner::LogLevel,
-};
+use dvcdbg::prelude::*;
+
+use dvcdbg::scanner::run_explorer;
 use embedded_io::Write;
 use panic_halt as _;
 
 adapt_serial!(UnoWrapper);
 
-// PrefixExecutor をそのまま使えるようにコピー
-struct PrefixExecutor {
-    prefix: u8,
-    init_sequence: heapless::Vec<u8, 64>,
-    buffer: heapless::Vec<u8, 64>,
-}
 
-impl PrefixExecutor {
-    fn new(prefix: u8, init_sequence: heapless::Vec<u8, 64>) -> Self {
-        Self {
-            prefix,
-            init_sequence,
-            buffer: heapless::Vec::new(),
-        }
-    }
-}
-
-impl<I2C> CmdExecutor<I2C> for PrefixExecutor
-where
-    I2C: dvcdbg::compat::I2cCompat,
-    <I2C as dvcdbg::compat::I2cCompat>::Error: dvcdbg::compat::HalErrorExt,
-{
-    fn exec(&mut self, i2c: &mut I2C, addr: u8, cmd: &[u8]) -> bool {
-        // init sequence before explorer commands
-        for &c in self.init_sequence.iter() {
-            let command = [self.prefix, c];
-            if i2c.write(addr, &command).is_err() {
-                return false;
-            }
-        }
-
-        self.buffer.clear();
-        if self.buffer.push(self.prefix).is_err() || self.buffer.extend_from_slice(cmd).is_err() {
-            return false;
-        }
-
-        i2c.write(addr, &self.buffer).is_ok()
-    }
-}
-
-// SH1107G 初期化コマンド
+// SH1107G 安全初期化コマンド（順番に送る）
 const SH1107G_NODES: &[CmdNode<'_>] = &[
     CmdNode { bytes: &[0xAE], deps: &[] },         // Display OFF
-    CmdNode { bytes: &[0xDC, 0x00], deps: &[] },   // Display start line = 0
+    CmdNode { bytes: &[0xDC, 0x00], deps: &[0xAE] },   // Display start line = 0
     CmdNode { bytes: &[0x81, 0x2F], deps: &[] },   // Contrast
     CmdNode { bytes: &[0x20, 0x02], deps: &[] },   // Memory addressing mode: page
     CmdNode { bytes: &[0xA0], deps: &[] },         // Segment remap normal
-    CmdNode { bytes: &[0xC0], deps: &[] },         // Common output scan direction normal
-    CmdNode { bytes: &[0xA4], deps: &[] },         // Entire display ON from RAM
-    CmdNode { bytes: &[0xA6], deps: &[] },         // Normal display
+    CmdNode { bytes: &[0xC0], deps: &[0xA0] },         // Common output scan direction normal
+    CmdNode { bytes: &[0xA4], deps: &[0xC0] },         // Entire display ON from RAM
+    CmdNode { bytes: &[0xA6], deps: &[0xA4] },         // Normal display
     CmdNode { bytes: &[0xA8, 0x7F], deps: &[] },   // Multiplex ratio 128
     CmdNode { bytes: &[0xD3, 0x60], deps: &[] },   // Display offset
     CmdNode { bytes: &[0xD5, 0x51], deps: &[] },   // Oscillator frequency
@@ -101,12 +60,10 @@ fn main() -> ! {
         LogLevel::Verbose,
     );
 
-    let mut executor = PrefixExecutor::new(0x00, init_sequence);
-
     // Explorer 実行
-    let _ = explorer.explore(&mut i2c, &mut serial_wrapper, &mut executor);
-
-    let _ = writeln!(serial_wrapper, "[oled] Init sequence applied");
+    // The `run_explorer` function internally creates a `PrefixExecutor`
+    let _ = run_explorer(&explorer, &mut i2c, &mut serial_wrapper, &init_sequence.clone(), 0x00, LogLevel::Verbose);
+    let _ = writeln!(serial_wrapper, "[oled] init sequence applied");
 
     loop {
         delay.delay_ms(1000u16);

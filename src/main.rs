64ae413b3f957e97f6e95ff6df08ee6f3905224d
Placeless <thinkoverheat@gmail.com>
2025-09-01 22:33:44 +0900

    fix by cline

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@
 #![no_main]
 
 use core::fmt::Write;
-use dvcdbg::compat::util::calculate_cmd_buffer_size;
-use dvcdbg::explore::explorer::{CmdNode, Explorer};
 use dvcdbg::prelude::*;
+use dvcdbg::nodes; // nodes!マクロをインポート
 use panic_abort as _;
 
 adapt_serial!(UnoWrapper);
@@ -24,48 +23,123 @@ fn main() -> ! {
         100_000,
     );
 
+    writeln!(serial, "Starting I2C check...").ok();
+
     match i2c.write(0x3C, &[0x00]) {
         Ok(_) => writeln!(serial, "I2C OK.").ok(),
         Err(_) => writeln!(serial, "I2C failed.").ok(),
     };
     arduino_hal::delay_ms(1000);
 
-    // --- Explorer nodes ---
-    const INIT_SEQUENCE_LEN: usize = 22;
-    let prefix: u8 = 0x00;
+    const PREFIX: u8 = 0x00;
 
-let res = factorial_sort! {
-    prefix,
-    init = [
+    // --- Explorerノード定義 ---
+    const INIT_SEQUENCE: [u8; 22] = [
         0xAE, 0xD5, 0x51, 0xA8, 0x3F, 0xD3, 0x60, 0x40,
         0x00, 0xA1, 0x00, 0xA0, 0xC8, 0xAD, 0x8B, 0xD9,
         0x22, 0xDB, 0x35, 0x8D, 0x14, 0xB0
-    ],
-    nodes = [
-        [0xAE],
-        [0xD5, 0x51] @ [0],
-        [0xA8, 0x3F] @ [1],
-        [0xD3, 0x60] @ [2],
-        [0x40] @ [3],
-        [0xA1] @ [4],
-        [0xA0] @ [5],
-        [0xC8] @ [6],
-        [0xAD, 0x8B] @ [7],
-        [0xD9, 0x22] @ [8],
-        [0xDB, 0x35] @ [9],
-        [0x8D, 0x14] @ [10],
-        [0x20, 0x00] @ [11],
-        [0xA6] @ [12],
-        [0xAF] @ [13]
-    ],
-    &mut i2c,
-    &mut serial
-};
+    ];
 
-match res {
-    Ok(()) => writeln!(serial, "[I] Explorer OK.").ok(),
-    Err(e) => writeln!(serial, "[E] Explorer failed: {:?}\r\n", e).ok(),
-};
+    // -----------------------
+    // --- 実行パターン切り替え ---
+    // -----------------------
+
+    // 1) factorial explorer
+    let (explorer_instance, _executor_instance) = nodes! {
+        prefix = PREFIX,
+        [
+            [0xAE],
+            [0xD5, 0x51] @ [0],
+            [0xA8, 0x3F] @ [1],
+            [0xD3, 0x60] @ [2],
+            [0x40] @ [3],
+            [0xA1] @ [4],
+            [0xA0] @ [5],
+            [0xC8] @ [6],
+            [0xAD, 0x8B] @ [7],
+            [0xD9, 0x22] @ [8],
+            [0xDB, 0x35] @ [9],
+            [0x8D, 0x14] @ [10],
+            [0x20, 0x00] @ [11],
+            [0xA6] @ [12],
+            [0xAF] @ [13]
+        ]
+    };
+
+    const NODES_COUNT: usize = 15; // 手動で再定義
+    const CMD_BUFFER_SIZE: usize = 3; // 手動で再定義
+
+    const INIT_SEQUENCE_LEN: usize = INIT_SEQUENCE.len();
+
+    // let res = factorial_sort! {
+    //     explorer_instance,
+    //     &mut i2c,
+    //     &mut serial,
+    //     PREFIX,
+    //     &INIT_SEQUENCE,
+    //     NODES_COUNT,
+    //     INIT_SEQUENCE_LEN,
+    //     CMD_BUFFER_SIZE
+    // };
+
+    /*
+    // 2) pruning explorer
+    let res = pruning_sort! {
+        prefix = PREFIX,
+        init = init_sequence,
+        nodes = [
+            [0xAE],
+            [0xD5, 0x51] @ [0],
+            [0xA8, 0x3F] @ [1],
+            [0xD3, 0x60] @ [2],
+            [0x40] @ [3],
+            [0xA1] @ [4],
+            [0xA0] @ [5],
+            [0xC8] @ [6],
+            [0xAD, 0x8B] @ [7],
+            [0xD9, 0x22] @ [8],
+            [0xDB, 0x35] @ [9],
+            [0x8D, 0x14] @ [10],
+            [0x20, 0x00] @ [11],
+            [0xA6] @ [12],
+            [0xAF] @ [13]
+        ],
+        &mut i2c,
+        &mut serial
+    };
+    */
+
+    /*
+    // 3) single topological sort
+    let res = get_one_sort! {
+        prefix = PREFIX,
+        init = init_sequence,
+        nodes = [
+            [0xAE],
+            [0xD5, 0x51] @ [0],
+            [0xA8, 0x3F] @ [1],
+            [0xD3, 0x60] @ [2],
+            [0x40] @ [3],
+            [0xA1] @ [4],
+            [0xA0] @ [5],
+            [0xC8] @ [6],
+            [0xAD, 0x8B] @ [7],
+            [0xD9, 0x22] @ [8],
+            [0xDB, 0x35] @ [9],
+            [0x8D, 0x14] @ [10],
+            [0x20, 0x00] @ [11],
+            [0xA6] @ [12],
+            [0xAF] @ [13]
+        ],
+        &mut i2c,
+        &mut serial
+    };
+    */
+
+    match res {
+        Ok(()) => writeln!(serial, "[I] Explorer OK.").ok(),
+        Err(e) => writeln!(serial, "[E] Explorer failed: {:?}\r\n", e).ok(),
+    };
 
     writeln!(serial, "Enter main loop.").ok();
     loop {
